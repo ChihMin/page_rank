e767402b8019124f5e011b9ca36b5fe6da4af0e8
ChihMin <s102062111@wlan.nthu.edu.tw>
2016-05-28 23:10:42 +0800

    1. fix error value of ranking 2. fix error edges of graph

--- HW2_102062111_Spark/src/main/scala/WordCount.scala
+++ HW2_102062111_Spark/src/main/scala/WordCount.scala
@@ -25,15 +25,35 @@ object WordCount {
         try { hdfs.delete(new Path(outputPath), true) } catch { case _ : Throwable => { } }
         
         val lines = sc.textFile(files)
-        val titleReg = "<title>(.+?)</title>"
-        val titleSplitReg = "<title>|</title>"
-        val originGraph = lines.flatMap(line =>{
+        val masterGraph = lines.flatMap(line =>{
+          val titleReg = "<title>(.+?)</title>"
+          val titleSplitReg = "<title>|</title>"
+          val m = Pattern.compile(titleReg).matcher(line)
+          val isFind = m.find()
+          val titleStr = m.group().split(titleSplitReg)
+        
+          titleStr.filter(_.length() != 0).map(word => {
+            val title = word.replaceAll("&quot;", "\"")
+                            .replaceAll("&gt;", ">")
+                            .replaceAll("&lt;", "<")
+                            .replaceAll("&amp;","&")
+                            .replaceAll("&apos;", "'")
+            
+            val nextNodes = new Page(List[String]())
+            nextNodes.setMaster()
+            (title, nextNodes)
+          })
+        }).cache
+
+        val slaveGraph = lines.flatMap(line =>{
+          val titleReg = "<title>(.+?)</title>"
+          val titleSplitReg = "<title>|</title>"
           val m = Pattern.compile(titleReg).matcher(line)
           val mNode = Pattern.compile("\\[\\[(.+?)([\\|#]|\\]\\])").matcher(line)
           val isFind = m.find()
           val titleStr = m.group().split(titleSplitReg)
         
-          titleStr.filter(_.length() != 0).map(word => {
+          titleStr.filter(_.length() != 0).flatMap(word => {
             val title = word.replaceAll("&quot;", "\"")
                             .replaceAll("&gt;", ">")
                             .replaceAll("&lt;", "<")
@@ -47,21 +67,38 @@ object WordCount {
                                   .replaceAll("&gt;", ">")
                                   .replaceAll("&lt;", "<")
                                   .replaceAll("&amp;","&")
-                                  .replaceAll("&apos;", "'")
+                                  .replaceAll("&apos;", "'").capitalize
                 allMatches = nextNode.capitalize :: allMatches
               }
             }
-            val nextNodes = new Page(allMatches)
-            (title, nextNodes)
+
+            allMatches.map(nextNode => {
+              val preNode = List(title)
+              (nextNode, new Page(preNode))
+            })
           })
         })
+        
+        val originGraph = slaveGraph.union(masterGraph)
+                          .reduceByKey(_++_)
+                          .filter(node => node._2.getRole() == 0)
+                          .flatMap(node => {
+                            node._2.getEdges().map(preNode => {
+                              (preNode, new Page(List[String](node._1)))
+                            })
+                          })
+                          .union(masterGraph)
+                          .reduceByKey(_++_)
+        masterGraph.unpersist()
+        //originGraph.saveAsTextFile(outputPath)
+
 
         val N = originGraph.count
         var graph = originGraph.map(node => {
           node._2.setPageRank(Double.valueOf(1) / Double.valueOf(N))
           (node._1, node._2)
         }).persist()
-        
+
         val alpha = new Double(0.85)
         var error = new Double(1)
         val one = new Double(1)
@@ -107,8 +144,10 @@ object WordCount {
           graph = newGraph
           graph.persist()
            
-          println("[ERROR DIFF] " + String.valueOf(error))
+          println("[ERROR DIFF] " + String.valueOf(error) + ", Iteration = " + String.valueOf(numOfIteration))
         } 
+ //
+ //
         val finalGraph = graph.sortBy {
           node => (-node._2.getPageRank(), node._1)
         }
@@ -116,24 +155,29 @@ object WordCount {
           String.valueOf(node._1) + "\t" + node._2.toString()
         )
         finalOutput.saveAsTextFile(outputPath)
+
         sc.stop
     }
 }
 
 class Page(lk: List[String]) extends Serializable {
-  val link: List[String] = lk
+  var link: List[String] = lk
   var pageRank: Double = 0.0
+  var role: Int = 1
 
   override def toString(): String = {
     val linkStr = link match {
       case null => "(NULL)"
       case _ => link.toString()
     }
-    //linkStr + "\t" + String.valueOf(pageRank)
+    //linkStr + "\t" + String.valueOf(pageRank) + "\t [ROLE] = " + String.valueOf(role)
     String.valueOf(pageRank)
   }
   def setPageRank(pg: Double): Unit = {
     pageRank = pg
+  }
+  def setEdges(edge: List[String]): Unit = {
+    link = edge
   }
   def getPageRank(): Double = pageRank
   def getNumOfEdges(): Int = link.length
@@ -150,8 +194,26 @@ class Page(lk: List[String]) extends Serializable {
     page.setPageRank(sum)
     page
   }
+  
+  def ++(that: Page): Page = {
+    var master = this
+    var slave = that
+    if (that.getRole() == 0) {
+      master = that
+      slave = this
+    }
+    val edges = master.getEdges() ::: slave.getEdges()
+    master.setEdges(edges)
+    master
+  }
 
   def getPageRankDiff(pg: Double): Double = {
     new Double(Math.abs(this.pageRank  - pg)) 
   }
+  
+  def setMaster(): Unit = {
+    this.role = 0
+  }
+
+  def getRole(): Int = this.role
 }
